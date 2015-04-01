78d75d861fd076e944ca8a83642cc58a4fbc4065
Xiaopow <xiaoyang88@gmail.com>
2015-04-01 18:07:05 +0800

    add control panel

--- js/app.js
+++ js/app.js
@@ -2,18 +2,32 @@
   var option = {
     time: 12,
     sessions: 4,
-    homeName: 'Home',
-    awayName: 'Away',
-    homePlayers: [],
-    awayPlayers: []
   }
+
+  var homeTeam = {
+    name: 'Home',
+    players: [],
+    score: 0,
+    teamFoul: 0,
+    timeout: 5
+  }
+
+  var awayTeam = {
+    name: 'Home',
+    players: [],
+    score: 0,
+    teamFoul: 0,
+    timeout: 5
+  }
+
   var minLeft = option.time;
   var secLeft = 0;
   var msecLeft = 0;
   var timer;
-  var homeScore = 0;
-  var awayScore = 0;
   var shotSecLeft = 24;
+  var shotTimer;
+  var timeOutTimeLeft = 0;
+  var timeOutTimer;
 
   //---------------------------- Timer System ----------------------------
 
@@ -60,29 +74,107 @@
 
   var shotClock = function () {
     if (shotSecLeft == 0) {
+      $('#shot-clock').text('00')
       shotSecLeft = 24;
-      
+    } else if (shotSecLeft > 0) {
+      if (shotSecLeft > 9) {
+        $('#shot-clock').text(shotSecLeft);
+        shotSecLeft = shotSecLeft - 1;
+      } else {
+        $('#shot-clock').text('0' + shotSecLeft);
+        shotSecLeft = shotSecLeft - 1;
+      }
     }
   }
 
+  var resumeShotClock = function() {
+    shotTimer = setInterval(shotClock, 1000);
+  }
+
+  var pauseShotClock = function() {
+    clearInterval(shotTimer);
+  }
+
+  var resetShotClock = function() {
+    pauseShotClock();
+    shotSecLeft = 23;
+    $('#shot-clock').text('24');
+    resumeShotClock();
+  }
+
+  var resetShotClockLeft = function() {
+    shotSecLeft = 23;
+    $('#shot-clock').text('24');
+  }
   //---------------------------- Scoring System ----------------------------
 
   var addScore = function(team,point) {
     if (team == "home") {
-      homeScore = homeScore + point;
-      if (homeScore > 9) {
-        $('#home-score').text(homeScore);
+      homeTeam.score = homeTeam.score + point;
+      if (homeTeam.score > 9) {
+        $('#home-score').text(homeTeam.score);
       } else {
-        $('#home-score').text('0' + homeScore);
+        $('#home-score').text('0' + homeTeam.score);
       }
     } else if (team == "away") {
-      awayScore = awayScore + point;
-      if (awayScore > 9) {
-        $('#away-score').text(awayScore);
+      awayTeam.score = awayTeam.score + point;
+      if (awayTeam.score > 9) {
+        $('#away-score').text(awayTeam.score);
       } else {
-        $('#away-score').text('0' + awayScore);
+        $('#away-score').text('0' + awayTeam.score);
       }
     }
   }
+  //---------------------------- Time Out System ----------------------------
 
+  var timeOutCountDown = function() {
+    if (timeOutTimeLeft == 0) {
+      console.log("stop time out");
+      clearInterval(timeOutTimer);
+      $('#shot-clock').text('0');
+      setTimeout(function(){
+        if (shotSecLeft < 24) {
+          if (shotSecLeft > 8) {
+            $('#shot-clock').text(shotSecLeft+1);
+          } else {
+            $('#shot-clock').text('0' + (shotSecLeft+1));
+          } 
+        } else {
+            $('#shot-clock').text(shotSecLeft);
+        }        
+      },1000);
+    } else {
+      $('#shot-clock').text(timeOutTimeLeft);
+      timeOutTimeLeft = timeOutTimeLeft -1;
+    }
+  }
+
+  var callTimeOut = function(team,time) {
+    if (team == 'home') {
+      homeTeam.timeout = homeTeam.timeout - 1;
+      $('#home-time-out').text(homeTeam.timeout);
+    } else if (team == 'away') {
+      awayTeam.timeout = awayTeam.timeout - 1;
+      $('#away-time-out').text(awayTeam.timeout);
+    }
+    pauseShotClock();
+    pauseTimer();
+    timeOutTimeLeft = time;
+    timeOutTimer = setInterval(timeOutCountDown, 1000);
+  }
+  //--------------------------- Team Foul System ----------------------------
+
+
+  //--------------------------- Overall Controls ----------------------------
+
+  var resumeAll = function() {
+    resumeTimer();
+    resumeShotClock();
+  }
+
+  var pauseAll = function() {
+    pauseTimer();
+    pauseShotClock();
+  }
+  
 // })
